65b95843a2e5ae811364b319ef2f577f57caa72a
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-09 03:15:43 +0900

    Added __static staticディレクトリの動作サンプル

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,8 +1,17 @@
-const electron = require('electron');
+import electron from 'electron';
+import fs from 'fs';
+import path from 'path';
 
 process.once('loaded', () => {
+
   console.log('---- preload.js loaded ts ----');
   // globalに入れるとwindowオブジェクトのプロパティに設定される
   (global as any).electron = electron;
   (global as any).module   = module;
+  (global as any).test     = () => {
+    const pathToAsset = path.join(__static, '/foobar.txt');
+    const fileContents = fs.readFileSync(pathToAsset, 'utf8');
+    return fileContents;
+  }
+  
 });
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import './styles.scss'
 
-
+alert((window as any).test());
 const Header = <h1>hello electron webpack</h1>;
 
 ReactDOM.render(Header, document.getElementById("app"));
