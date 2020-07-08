1f36eabc7a259a0b4c8c25fad4f4cb2971aba3b8
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-09 03:57:19 +0900

    Tested react-router-dom mobx

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,9 +1,75 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { observable, computed, action  } from 'mobx';
+import { observer } from 'mobx-react';
 import './reset.css';
 import './styles.scss';
 
-alert((window as any).test());
-const Header = <h1>hello electron webpack</h1>;
+class Store {
+  @observable private _count = 0;
 
-ReactDOM.render(Header, document.getElementById("app"));
+  @computed get count() {
+    return this._count;
+  }
+
+  @action add() {
+    this._count++;
+  }
+
+  @action sub() {
+    this._count--;
+  }
+}
+const store = new Store();
+
+@observer
+class Sample1 extends React.Component {
+  render() {
+    return (
+      <div>
+        count = {store.count}
+        <Link to="/sample2">sample2へ</Link>
+      </div>);
+  }
+}
+
+@observer
+class Sample2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick={ () => {store.add();}}>Add</button>
+        <button onClick={ () => {store.sub();}}>Sub</button>
+        <Link to="/">Home</Link>
+      </div>);
+  }
+}
+
+class App extends React.Component {
+  constructor(props:any) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>hello electron webpack</h1>
+        <p>
+          「{(window as any).test()}」は static/foobar.txt に書かれている内容です。
+        </p>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Sample1} />
+            <Route exact path="/sample2" component={Sample2} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
+
+
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
